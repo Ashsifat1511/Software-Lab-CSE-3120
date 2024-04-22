71215a971550d4e3af6177e34b8bcd9a13aa58c5
Ashsifat1511 <sifatashrarul@gmail.com>
2024-04-22 11:48:53 +0600

    Integration Test

--- Lab-06/src/main/java/org/example/Main.java
+++ Lab-06/src/main/java/org/example/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Account account1 = new Account(101, 1000.0);
         Account account2 = new Account(102, 500.0);
-        boolean transferAmountSuccess = Transaction.transferAmount(account1, account2, 200.0);
-
+        boolean transferAmountSuccess = Transaction.transferAmount(account1, account2, 300.0);
     }
 }
 
